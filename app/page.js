d54d978ebbd53ebac75a2f0bafccef3a9709e142
ELOUAIZI Ayoub <ayoubelouaizi@gmail.com>
2023-11-20 02:09:31 +0100

    review card

--- app/page.js
+++ app/page.js
@@ -1,11 +1,25 @@
-import Hero1 from '../components/Hero1'
-import Image from 'next/image'
+import Hero1 from "../components/Hero1";
+import Image from "next/image";
+import ProductCard from "../components/ProductCard";
 
 export default function Home() {
   return (
     // flex min-h-screen flex-col items-center justify-between p-24
     <main className="">
-    <Hero1/>
+      <Hero1 />
+      <div className="lg:flex">
+        <div className=" m-5">
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+        </div>
+        <div className="bg-blue-300  w-1/5 border-black p-2 m-2"> </div>
+        
+      </div>
     </main>
-  )
+  );
 }
