f0fdf4966265c418ba4f1e9f7ab9e8031b986ff2
ELOUAIZI Ayoub <ayoubelouaizi@gmail.com>
2023-11-25 17:39:42 +0100

    target blanc

--- app/components/ReviewCard.jsx
+++ app/components/ReviewCard.jsx
@@ -17,6 +17,7 @@ const ReviewCard = ({ review }) => {
       <a
         className="group inline-flex items-center h-9 rounded-full text-sm font-semibold whitespace-nowrap px-3 focus:outline-none focus:ring-2 bg-slate-100 text-slate-700 hover:bg-slate-200 hover:text-slate-900 focus:ring-slate-500 dark:bg-slate-700 dark:text-slate-100 dark:hover:bg-slate-600 dark:hover:text-white dark:focus:ring-slate-500 mt-6"
         href={review.link}
+        target="_blank"
       >
         Learn more
         <svg
